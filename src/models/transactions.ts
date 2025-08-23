b4d042aa93b41b90445d0c4db99768484da3c62e
AlexSilvak <alex.silvak23@gmail.com>
2025-08-23 14:00:43 -0300

    feat: melhorado tela DRE

--- src/models/transactions.ts
+++ src/models/transactions.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema, Document, models } from 'mongoose'
 interface ITransaction {
   trntype: string
   amount: number
-  payment_method?: string
+  payment_method:string
   date: Date
   memo?: string
   fitid: string
@@ -14,7 +14,6 @@ export interface ITransactionDoc extends Document {
   account_id: string
   bank_id: string
   branch_id: string
-  payment_method?: string
   transactions: ITransaction[]
   imported_at: Date
   user_id?: string;
@@ -23,7 +22,7 @@ export interface ITransactionDoc extends Document {
 const transactionSchema = new Schema<ITransaction>({
   trntype: { type: String, required: true },
   amount: { type: Number, required: true },
-  payment_method: { type: String, default: '' },
+  payment_method:{ type: String, required: true },
   date: { type: Date, required: true },
   memo: { type: String, default: '' },
   fitid: { type: String, required: true, unique: true },
@@ -35,7 +34,6 @@ const transactionsSchema = new Schema<ITransactionDoc>(
     account_id: { type: String, required: true },
     bank_id: { type: String, required: true },
     branch_id: { type: String, required: true },
-    payment_method: { type: String, default: '' },
     transactions: { type: [transactionSchema], default: [] },
     imported_at: { type: Date, default: Date.now },
     user_id: { type: String, default: "" },
